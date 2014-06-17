3574fcd4fd21a8947f6b90eaa892b98db774a965
root <root@pplxconfig3.physics.ox.ac.uk>
2014-06-17 13:25:16 +0100

    Tested against a real server

---
+++ manifests/params.pp
@@ -0,0 +1,8 @@
+class lustre_server::params (
+    $majorversion=hiera("lustre_server::params::majorversion","2.1") ,
+    $minorversion=hiera("lustre_server::params::minorversion","6") ,
+    $kkernelversion=hiera("lustre_server::params::kkernelversion","2.6.32-358.11.1.el6_lustre") ,
+    $lkernelversion=hiera("lustre_server::params::lkernelversion","2.6.32_358.11.1.el6_lustre.x86_64") 
+)
+{   
+}
