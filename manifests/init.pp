56cf303d895eb782f9dc4ee3353683219996e520
Sean Brisbane <s.brisbane1@physics.ox.ac.uk>
2016-05-31 19:11:00 +0100

    repo server is mirror.physics

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,20 +1,28 @@
 class lustre_server ( $majorversion=$lustre_server::params::majorversion,
                       $minorversion=$lustre_server::params::minorversion,
 		      $kkernelversion=$lustre_server::params::kkernelversion,
-                      $lkernelversion=$lustre_server::params::lkernelversion 
+                      $lkernelversion=$lustre_server::params::lkernelversion,
+                      $fkernelversion=$lustre_server::params::fkernelversion,
+                      $kernelsuffix=$lustre_server::params::kernelsuffix,
+                      $repo=$lustre_server::params::repo, 
   ) inherits lustre_server::params {
   notify {"Setting up lustre server with lustreversion $majorversion" : }
+
+  if ( "${repo}" == "default" ) {
+    $reposource="puppet:///modules/lustre_server/oxford-local-lustre-${majorversion}-server.repo" 
+  
+  } 
+  else {
+    $reposource=$repo
+  }
   #Paranoia
   if ($majorversion=="2.1") {
-
-
     file { "/etc/yum.repos.d/oxford-local-lustre-${majorversion}-server.repo":
     ensure  => present,
     mode    => '0644',
     owner   => 'root',
     group   => 'root',
     source  => "puppet:///modules/lustre_server/oxford-local-lustre-${majorversion}-server.repo", 
-    
     }
   }
   if ($majorversion=="2.5") {
@@ -23,30 +31,52 @@ class lustre_server ( $majorversion=$lustre_server::params::majorversion,
     mode    => '0644',
     owner   => 'root',
     group   => 'root',
-    source  => "puppet:///modules/lustre_server/oxford-local-lustre-${majorversion}-server.repo", 
-    
+    source  => "${reposource}", 
     }
   }
+  if ($majorversion=="testing") {
+    file { "/etc/yum.repos.d/oxford-local-lustre-${majorversion}-server.repo":
+    ensure  => present,
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    source  => "${reposource}",
+    }
+
+  }
+  file { "/usr/local/bin/lustre_bootstrap":
+    ensure  => present,
+    mode    => '0744',
+    owner   => 'root',
+    group   => 'root',
+    content => template("$module_name/lustre_bootstrap.erb")
+  }
+
   $packages = [ "*686" , "*386" ]
   package { $packages : ensure => absent }
   #The name scheme is slightly different for kernel and lustre packages (- -->_)
 
-  $pinnedkpackages = [ "kernel-${kkernelversion}", "kernel-firmware-${kkernelversion}",]
+  $pinnedkpackages = [ "kernel", ]
+  $pinnedkfpackages = [  "kernel-firmware",]
   $pinnedlkpackages = [ "lustre", "lustre-modules", ]
   $pinnedkopackages = [ "perf", ]
 
   package { $pinnedkpackages : 
-            ensure => "installed",
+            ensure => "${kkernelversion}${kernelsuffix}",
 	    require => File["/etc/yum.repos.d/oxford-local-lustre-${majorversion}-server.repo"] ,
           }
 
+  package { $pinnedkfpackages : 
+            ensure => "${fkernelversion}",
+	    require => File["/etc/yum.repos.d/oxford-local-lustre-${majorversion}-server.repo"] ,
+          }
   package { $pinnedlkpackages :
             ensure => "${majorversion}.${minorversion}-${lkernelversion}",
             require => [Package[$pinnedkpackages], File["/etc/yum.repos.d/oxford-local-lustre-${majorversion}-server.repo"] ]
           }
 
   package { $pinnedkopackages :
-            ensure => "${kkernelversion}",
+            ensure => "${fkernelversion}",
             require => File["/etc/yum.repos.d/oxford-local-lustre-${majorversion}-server.repo"],
   }
 
